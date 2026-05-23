64ed63ced16a976f5611a34dd9db4007855985e6
AlvinNdei <alvinmuriuki714@gmail.com>
2026-05-23 11:29:04 +0300

    Added the javascript Functions

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
-let button=document.getElementById('mybutton');
-button.addEventListener("click",function(){
-    alert('Your info was submitted');
-    console.log("A button was clicked");
-});
-let heading=document.querySelector("h1");
-heading.addEventListener('mouseover',function(){
-    heading.style.color="red";
-});
-heading.addEventListener('mouseout',function(){
-    heading.style.color="black";
-});
-let btt=document.querySelector("button");
-btt.addEventListener('mouseover',function(){
-    btt.style.color="blue";
-});
-btt.addEventListener('mouseout',function(){
-    btt.style.color="black";
-});
-document.addEventListener("keydown",function(event){
-    console.log('You pressed the ${event.key} key');
-    if (event.key==="Enter"){
-        console.log("Enter key pressed");
+const display=document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value+=input;
+}
+
+function clearDisplay(){
+    display.value="";
+}
+
+function calculate(){
+    try{
+        display.value=eval(display.value);
     }
-});
+    catch(error){
+        display.value="Error";
+    }
+    
+}
